9ede986baaca022c53d01ece65082de1fde57584
Sabrina Rodrigues <Sabrinadrivefatec@gmail.com>
2024-06-19 21:32:42 -0300

    trabalho completo

---
+++ Downloads/blogAPI/BlogAPIv1/BlogAPI/src/main/java/blogapi/dev/BlogAPI/beans/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package blogapi.dev.BlogAPI.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customAPI() {
+        return new OpenAPI().info(new Info().title("BlogAPI").version("1.0.0"));
+    }
+}
